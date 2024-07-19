47e719faa23fbd781d72f4242ffea77850c1dc6d
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2024-07-19 09:43:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     
     # database config
-    DB_NAME = os.environ.get("DB_NAME","Digital_Rename_Bot")     
+    DB_NAME = os.environ.get("DB_NAME","rename-premium")     
     DB_URL = os.environ.get("DB_URL","")
  
     # other configs
